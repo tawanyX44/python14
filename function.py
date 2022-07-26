e126dae985e0504628ed391a8999f111f932c999
anongrat <50249@cru.ac.th>
2022-07-26 14:41:23 +0700

    function

---
+++ function.py
@@ -0,0 +1,13 @@
+#สร้างฟังก์ชัน
+def hello():
+    print("Hello world")
+    print("Anongrat")
+#เรียกใช้งานฟังก์ชัน
+hello()
+#ส่งค่าผ่านparameter+return
+def sum(x,y):
+    x + y
+    return "hello"
+    
+print(sum(5,8))
+#
---
+++ work-function.py
@@ -0,0 +1,16 @@
+  #คำสั่งหาพื้นที่สี่เหลี่ยม
+def square(width,height):
+    ans = width*height
+    print (ans)
+square(4,4)
+#คำสั่งหาพื้นที่สามเหลี่ยม
+def triangle(base,height):
+    ans = 1/2*base*height
+    print (ans)
+triangle(5,12)
+#คำสั่งหาพื้นที่วงกลม
+def circle(radius):
+    ans = 3.14 * (radius*radius)
+    print (ans)
+circle(7)
+#อนงค์รัตน์ อินทวารี ม.6/14 เลขที่44
